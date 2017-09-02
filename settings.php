f23eeb06e397fbd6c9b818393231a4e3314f2754
RogerVigo <rogercalonso@gmail.com>
2017-09-02 19:56:07 +0100

    Adding curl to settings

--- settings.php
+++ settings.php
@@ -6,9 +6,9 @@ if ($ADMIN->fulltree) {
 
     $connect = get_string('connect', 'smartclassroom');
     $template = "
-<button type=\"button\" class=\"get_token_button\">{$connect}</button>
 <script type=\"text/javascript\">
 //<![CDATA[
+console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 YUI().use('node', 'io', 'dump', 'json-parse', 'io-xdr', function(Y){
 	Y.one('.get_token_button').on('click', function(e){
 	var config = {
@@ -38,18 +38,29 @@ YUI().use('node', 'io', 'dump', 'json-parse', 'io-xdr', function(Y){
 ";    
     
 	$settings->add(new admin_setting_configtext('SmartClassRoom_API_key', get_string('apikey', 'smartclassroom'),
-            get_string('setapikey', 'smartclassroom'), "smart-xunta", PARAM_STR));
+            get_string('setapikey', 'smartclassroom'), "smart-xunta", PARAM_TEXT));
 	$settings->add(new admin_setting_configtext('SmartClassRoom_Secret', get_string('secret', 'smartclassroom'),
-            get_string('setsecret', 'smartclassroom'), "gave-chile-moment-wood", PARAM_STR));
+            get_string('setsecret', 'smartclassroom'), "gave-chile-moment-wood", PARAM_TEXT));
 	$settings->add(new admin_setting_configtext('SmartClassRoom_OAuth', get_string('oauthip', 'smartclassroom'),
-            get_string('setoauthip', 'smartclassroom'), "", PARAM_STR));
+            get_string('setoauthip', 'smartclassroom'), "", PARAM_TEXT));
 	$settings->add(new admin_setting_configtext('SmartClassRoom_Backoffice', get_string('backofficeip', 'smartclassroom'),
-            get_string('setbackofficeip', 'smartclassroom'), "", PARAM_STR));
+            get_string('setbackofficeip', 'smartclassroom'), "", PARAM_TEXT));
 	$settings->add(new admin_setting_configtext('SmartClassRoom_SCR', get_string('smartclassroomip', 'smartclassroom'),
-            get_string('setsmartclassroomip', 'smartclassroom'), "", PARAM_STR));
+            get_string('setsmartclassroomip', 'smartclassroom'), "", PARAM_TEXT));
+	try{
+		$curl = curl_init();
+	
+		curl_setopt($curl, CURLOPT_URL, "http://gradiant-dev-classroom.smarted.cloud:3065/api/v1/oauth/token?grant_type=client_credentials");
+		//curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic ' + btoa('smart-client:gave-chile-moment-wood'));
+	
+		$result = curl_exec($curl);
+		curl_close($curl);
+	}catch(Exception $e){
+		
+	}
 	$settings->add(new admin_setting_configtext('SmartClassRoom_Token', get_string('token', 'smartclassroom'),
-            get_string('authtoken', 'smartclassroom'), "", PARAM_STR));
-    $settings->add(new admin_setting_heading("token_section", "", $template));              
+            "<button type=\"button\" class=\"get_token_button\">{$connect}</button>", "", PARAM_TEXT));
 }
+
 
 
