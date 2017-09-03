28fb0c10d5df78455add4161edf9b0c56b152e2a
BZD <info@blazdevelopment.com>
2017-09-03 18:56:28 +0200

    Diccionario Español

---
+++ lang/es/smartclassroom.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for smartclassroom
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_smartclassroom
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'smartclassroom';
+$string['modulenameplural'] = 'smartclassrooms';
+$string['modulename_help'] = 'Use the smartclassroom module for... | The smartclassroom module allows...';
+$string['smartclassroom:addinstance'] = 'Añadir una nueva SmartClassRoom';
+$string['smartclassroom:submit'] = 'Enviar SmartClassRoom';
+$string['smartclassroom:view'] = 'Ver SmartClassRoom';
+$string['smartclassroomfieldset'] = 'Custom example fieldset';
+$string['smartclassroomname'] = 'Nombre SmartClassRoom';
+$string['smartclassroomname_help'] = 'This is the content of the help tooltip associated with the smartclassroomname field. Markdown syntax is supported.';
+$string['smartclassroom'] = 'SmartClassRoom';
+$string['pluginadministration'] = 'Administración de SmartClassRoom';
+$string['pluginname'] = 'smartclassroom';
+$string['unittype'] = 'Tipo de Unidad';
+$string['book'] = 'Libro';
+$string['unity'] = 'Unidad';
+$string['course'] = 'Curso';
+$string['firstprimary'] = 'Primary 1st';
+$string['secondprimary'] = 'Primary 2nd';
+$string['thirdprimary'] = 'Primary 3rd';
+$string['fourthprimary'] = 'Primary 4th';
+$string['fifthprimary'] = 'Primary 5th';
+$string['sixthprimary'] = 'Primary 6th';
+$string['firstsecundary'] = 'Secundary 1st';
+$string['secondsecundary'] = 'Secundary 2nd';
+$string['thirdsecundary'] = 'Secundary 3rd';
+$string['fourthsecundary'] = 'Secundary 4th';
+$string['maths'] = 'Matemáticas';
+$string['spanish'] = 'Español';
+$string['english'] = 'Inglés';
+$string['science'] = 'Ciencia';
+$string['first'] = 'Primero';
+$string['second'] = 'Segundo';
+$string['third'] = 'Tercero';
+$string['apikey'] = 'API Key';
+$string['setapikey'] = 'Introduzca API Key';
+$string['secret'] = 'SECRET';
+$string['setsecret'] = 'Introduzca SECRET';
+$string['oauthip'] = 'IP del Servidor OAuth';
+$string['setoauthip'] = 'Introduzca IP del Servidor OAuth';
+$string['backofficeip'] = 'IP del Servidor de BackOffice';
+$string['setbackofficeip'] = 'Introduzca IP del Servidor BackOffice';
+$string['smartclassroomip'] = 'IP del Servidor de SmartClassRoom';
+$string['setsmartclassroomip'] = 'Introduzca IP del Servidor de SmartClassRoom';
+$string['token'] = 'Token de Autenticación';
+$string['authtoken'] = 'Token de Autenticación Recibido';
+$string['tokensection'] = 'Sección Token';
+$string['connect'] = 'Conectar';
+$string[''] = '';
